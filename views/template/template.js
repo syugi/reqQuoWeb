3c080366f04f40f903676e79731e0cef367a957b
a슈기 <altnr23@gmail.com>
2020-06-03 03:06:19 +0000

    오픈그래프 추가

--- views/template/template.js
+++ views/template/template.js
@@ -14,7 +14,15 @@ exports.HTML = function(title, link, body , script){
     <link rel="stylesheet" href="/build/tailwind.css">
 
 		<title>${title}</title>
-    <meta name="description" content="20년 경력 전문가, 전문건설업 등록업체, 최첨단 전문 장비 보유, 시공후 2년 무료 A/S, 누수로 인한 고민 완벽해결, 인천 부천 전지역 가능">
+
+    <meta property="og:type" content="website"> 
+    <meta property="og:title" content=${title}>
+    <meta property="og:description" content="20년 경력 전문가, 전문건설업 등록업체, 최첨단 전문 장비 보유, 시공후 2년 무료 A/S, 누수로 인한 고민 완벽해결, 인천 부천 전지역 가능">
+    <!--<meta property="og:image" content="http://www.mysite.com/myimage.jpg">-->
+    <meta property="og:url" content="http://www.hknusu.com">
+    <meta name="description" content="20년 경력 전문가, 전문건설업 등록업체, 최첨단 전문 장비 보유, 시공후 2년 무료 A/S, 누수로 인한 고민 완벽해결, 인천 부천 전지역 가능">
+      
+      
 		${link}
 
 		<!--tailwind-->
